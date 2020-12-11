b847a4254330f5a189a956c2e5fa8b4097167914
FELIX <felixkautz@gmail.com>
2020-12-11 14:58:21 +0300

    Initial commit

---
+++ XML-SwiftUI/XML_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  XML_SwiftUIApp.swift
+//  XML-SwiftUI
+//
+//  Created by Felix Falkovsky on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct XML_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
